364d00aa78327dd0b2bef51e82cea249cb69bc47
meisner87 <87091390+meisner87@users.noreply.github.com>
2022-01-22 11:47:14 +0200

    [init] - first commit project - project initialization

---
+++ src/main/java/com/sda/javaee9springdemo/JavaEe9SpringApplication.java
@@ -0,0 +1,13 @@
+package com.sda.javaee9springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEe9SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEe9SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/javaee9springdemo/JavaEe9SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.javaee9springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEe9SpringdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
