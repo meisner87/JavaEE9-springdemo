70dcfe5f18c0acb1e8b3546e7e46b76a03de8647
meisner87 <87091390+meisner87@users.noreply.github.com>
2022-01-22 14:01:17 +0200

    [add] - our first controller

--- src/main/java/com/sda/javaee9springdemo/JavaEe9SpringApplication.java
+++ src/main/java/com/sda/javaee9springdemo/JavaEe9SpringApplication.java
@@ -3,6 +3,7 @@ package com.sda.javaee9springdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class JavaEe9SpringApplication {
 
