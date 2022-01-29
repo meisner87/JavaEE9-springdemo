39f7ae5561d987543ab5dce313439e0a8c6c3d42
meisner87 <87091390+meisner87@users.noreply.github.com>
2022-01-29 13:00:29 +0200

    [change] - some var examples in PersonController

--- src/main/java/com/sda/javaee9springdemo/contoller/PersonController.java
+++ src/main/java/com/sda/javaee9springdemo/contoller/PersonController.java
@@ -16,9 +16,20 @@ import java.util.List;
 public class PersonController {
 
     public static final String PERSONS_KEY = "personsKey";
-
+    //with var Java is going to quess type of the variable based on assigned value
+    //var name = "Andreas"; //String name = "Andreas";
     @GetMapping("/names")
     public String showListOfPersonsNames(Model data){
+//        // with var Java is going to guess type of the variable based on assigned value
+////        var name = "mariusz"; // String name = "mariusz";
+////        var mariusz = new Person("Mariusz", "Pastuszka", 18);
+////        Person mariusz = new Person("Mariusz", "Pastuszka", 18);
+////        var mariusz = null; // need initialization value, without that it won't work
+//        ArrayList<Person> myBestFriends = new ArrayList<Person>(); // till Java 7
+//        ArrayList<Person> myBestFriendsJava7 = new ArrayList<>(); // since Java 7
+//        var myBestFriendsJava10 = new ArrayList<Person>(); // since Java 10
+//        var myBestFriendsJava10v2 = new ArrayList<>(); // since Java 10
+//        var myBestFriendsJava10v3 = new ArrayList<Object>(); // since Java 10
     var persons
             = List.of(
             new Person("Andreas","Meisner",34),
