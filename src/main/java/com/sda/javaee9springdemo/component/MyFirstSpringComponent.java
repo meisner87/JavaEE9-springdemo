2af65771f8763ad5c7dfc52b8e9fb66c225526ac
meisner87 <87091390+meisner87@users.noreply.github.com>
2022-01-30 10:46:42 +0200

    [change] - examples of creating spring beans

---
+++ src/main/java/com/sda/javaee9springdemo/component/MyFirstSpringComponent.java
@@ -0,0 +1,9 @@
+package com.sda.javaee9springdemo.component;
+
+import org.springframework.stereotype.Component;
+
+@Component // request to Spring for creating object and storing inside Spring Context
+public class MyFirstSpringComponent {
+
+
+}
--- src/test/java/com/sda/javaee9springdemo/JavaEe9SpringdemoApplicationTests.java
+++ src/test/java/com/sda/javaee9springdemo/JavaEe9SpringdemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.sda.javaee9springdemo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,23 +14,65 @@ class Javaee9SpringApplicationTests {
     @Test
     void dependencyInjectionFirstTrial() {
         Toy plasticGun = new Toy("Plastic gun");
-        Child john = new Child("John",plasticGun);
+        Child john = new Child("John", plasticGun);
         // dependency injection is providing all required class dependencies during creation of objects of that class
         //plasticGun of type toy is dependency of john(of type Child)
         // john (which is of type Child) is dependency of jason (of type Parent)
         //dependency is actually composition,so one item is made of another
         Parent jason = new Parent("Jason", john);
+
     }
+
+    @Test
+    void recordsTest() {
+        Animal animal = new Animal();
+        System.out.println(animal);
+        //use records instead of @value from lombok
+        //records are immutable - cannot modify instances of the class - so setters
+        Toy plasticGun = new Toy("Plastic gun");
+        System.out.println(plasticGun);
+
+        //child.with(favoriteToy = plasticGun); //in future Java versions
+    }
+
+    @Test
+    void equalsTest() {
+        Assertions.assertTrue(4 == 4);
+
+        Animal one = new Animal();
+        Animal two = new Animal();
+        Animal three = one;
+
+        // == it checks if this is the same object
+        // equals by default it checks if this is the same object
+        Assertions.assertFalse(one == two);
+        Assertions.assertFalse(one.equals(two));
+
+        Toy plasticGun = new Toy("plastic gun");
+
 }
-record Toy(String name){}
-//Child is compose of : name and toy
-record Child(String name,Toy favoriteToy) {}
+
+//Toy is made of name
+record Toy(String name) {
+}
+
+//Child is composed of : name and toy
+record Child(String name, Toy favoriteToy) {
+}
+
 //Parent is made of(compose of) :name and child
 record Parent(String name, Child child) {
 }
-class Animal{}
-class Tiger extends Mammal{}
-class Mammal extends Animal{}
+
+class Animal {
+}
+
+class Tiger extends Mammal {
+}
+
+class Mammal extends Animal {
+}
+}
 //Tiger is Mammal
 //Tiger is Animal
-//we cannot say:Parent is Child(Parent dont extend Child)
+//we cannot say:Parent is Child(Parent don't extend Child)
