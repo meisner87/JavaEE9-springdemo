32588d50aced08f439f466d9521f609ec5da64e3
meisner87 <87091390+meisner87@users.noreply.github.com>
2022-02-12 13:38:53 +0200

    [add] - playing with ResponseEntity API continue

--- src/main/java/com/sda/javaee9springdemo/contoller/rest/FirstPersonRestController.java
+++ src/main/java/com/sda/javaee9springdemo/contoller/rest/FirstPersonRestController.java
@@ -2,6 +2,7 @@ package com.sda.javaee9springdemo.contoller.rest;
 
 import com.sda.javaee9springdemo.entity.Person;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,19 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class FirstPersonRestController {
 
 
-
     @GetMapping("/one-person")
-    public ResponseEntity<Person> onePerson(){
-        return ResponseEntity.ok(new Person("Andreas","Meisner",34));
-}
+    public ResponseEntity<Person> onePerson() {
+        return ResponseEntity.ok(new Person("Andreas", "Meisner", 34));
+    }
+
     @GetMapping("/one-person-404")
     public ResponseEntity<Person> onePerson404() {
         log.info("onePerson404");
-        return  ResponseEntity.notFound().build();
+        return ResponseEntity.notFound().build();
 
     }
+
     @GetMapping("/custom")
-    public ResponseEntity<Person> customResponseEntity(){
-        return new ResponseEntity<>(new Person("Andreas","Meisner",34), HttpStatus.OK);
+    public ResponseEntity<Person> customResponseEntity() {
+        return new ResponseEntity<>(new Person("Andreas", "Meisner", 34), HttpStatus.OK);
+    }
+
+    @GetMapping("/with-headers")
+    public ResponseEntity<Person> customResponseEntityWithHeaders(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("name","Andreas");
+        return new ResponseEntity<>(new Person("Toomas","Tamm",50),headers,HttpStatus.OK);
     }
 }
