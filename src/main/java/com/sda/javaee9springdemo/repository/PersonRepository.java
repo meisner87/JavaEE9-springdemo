7c61fcbb78222fc695fa24077015450ed8ed01ba
meisner87 <87091390+meisner87@users.noreply.github.com>
2022-02-13 12:40:24 +0200

    [change] - PersonEntity and crud repository added

--- src/main/java/com/sda/javaee9springdemo/repository/PersonRepository.java
+++ src/main/java/com/sda/javaee9springdemo/repository/PersonRepository.java
@@ -1,8 +1,12 @@
 package com.sda.javaee9springdemo.repository;
 
 
+import com.sda.javaee9springdemo.entity.PersonEntity;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class PersonRepository {
+public interface PersonRepository extends CrudRepository<PersonEntity,Long> {
+
+
 }
