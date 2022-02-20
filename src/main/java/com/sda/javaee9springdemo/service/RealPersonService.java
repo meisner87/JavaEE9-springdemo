822a3421651755cb04d7e4b2937906e537e393cb
meisner87 <87091390+meisner87@users.noreply.github.com>
2022-02-20 12:35:27 +0200

    [change] - configuration for rest template created

--- src/main/java/com/sda/javaee9springdemo/service/RealPersonService.java
+++ src/main/java/com/sda/javaee9springdemo/service/RealPersonService.java
@@ -5,6 +5,7 @@ import com.sda.javaee9springdemo.repository.PersonRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,10 +16,12 @@ public class RealPersonService {
 
 
     private final PersonRepository personRepository;
+    private final RestTemplate restTemplate;
 
 
-    public RealPersonService(PersonRepository personRepository) {
+    public RealPersonService(PersonRepository personRepository, RestTemplate restTemplate) {
         this.personRepository = personRepository;
+        this.restTemplate = restTemplate;
     }
 
     public List<PersonEntity> readAllPersonEntities() {
